30fa3b298550d7f77ad22fb869c0729be888dd85
Hiromichi Koeda <empty969@gmail.com>
2017-10-24 09:03:59 +0900

    ファイル内部のテキスト取得

--- getFileDescription.js
+++ getFileDescription.js
@@ -1,22 +1,30 @@
-// var fs = require("fs");
-// var path = require("path");
-//
-// var dir = './testDir';
-//
-// fs.readdir(dir, function(err, list) {
-//   list.forEach(function(file) {
-//     console.log(file);
-//   });
-// });
 var fs = require("fs");
 var file = 'url.txt';
-var replaceTargetStr = "/Users/hiromichikoeda/leihauoli/myGithub/";
+var replaceTargetStr = "/Users/hiromichikoeda/leihauoli/myGithub/getFileDescription/";
 
+// 正規表現参考:http://www.tohoho-web.com/js/regexp.html
+// http://yut.hatenablog.com/entry/20110305/1299318337
 var regExp = new RegExp(replaceTargetStr, "g");
 
-fs.readFile(file, function(err, data) {
-  var text = data.toString().replace(regExp,"");
-  var result = text.split(/\r\n|\r|\n/);
-  console.log(result);
-});
+var textToPaths = function (file) {
+	var text = fs.readFileSync(file).toString().replace(regExp, "");
+	var paths = text.split(/\r\n|\r|\n/);
+	return paths;
+}
+
+var getFileName = function (paths) {
+	paths.forEach(function (path, index) {
+		fs.readFile(path, 'utf8', function (err, data) {
+			if(err) {
+				console.log('エラーが発生しました');
+				throw err;
+			}
+			console.log(/画面名：(.*)/.test(data));
+			let pageName = data.match(/画面名：(.*)/)[1];
+			console.log(pageName);
+		});
+		// console.log(path);
+	});
+}
+getFileName(textToPaths(file));
 
